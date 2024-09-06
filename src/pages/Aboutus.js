6dbe0cf78a578f6df6cf2377dd418b389548d4c7
Mariana Sarovic <sarovicmariana@gmail.com>
2024-09-06 23:42:30 +0200

    Vymena hero sekce, stylovani burger menu a Aboutus

--- src/pages/Aboutus.js
+++ src/pages/Aboutus.js
@@ -1,21 +1,47 @@
+import misa from '../img/misa.jpg';
+import helena from '../img/helena.jpg';
+
 const Aboutus = () => {
   return (
     <div className="aboutus">
       <section className="paragraphs paragraphs-aboutus">
         <p>
-          Vítáme vás v salonu MVisage. Přijďte si odpočinout, dopřejte si péči o
-          vlasy i pleť a nechte se hýčkat v našem malém, útulném salonu na
-          pražských Vinohradech.
+          Vítáme vás v salonu <b>MVisage</b>. Přijďte si odpočinout, dopřejte si
+          péči o vlasy i pleť a nechte se hýčkat v našem malém, útulném salonu
+          na pražských Vinohradech.
         </p>
         <p>
-          Pracujeme s kosmetikou REDKEN a PAYOT a rádi vám poradíme, co je pro
-          vás nejlepší. Stále se učíme nové trendy, abychom vám mohli nabídnout
-          to nejlepší.
+          Pracujeme s kosmetikou <b>REDKEN</b> a <b>PAYOT</b> a rádi vám
+          poradíme, co je pro vás nejlepší. Stále se učíme nové trendy, abychom
+          vám mohli nabídnout to nejlepší.
         </p>
       </section>
-      <section>
-        <span className="name name-aboutus">Michaela Kantorová</span>
-        <span className="name name-aboutus">Helena Šubrtová</span>
+      <section className="figures">
+        <div className="figure-aboutus">
+          <figure className="figure">
+            <div className="img-profile-wrapper">
+              <img
+                className="img-profile img-profile-michaela"
+                src={misa}
+                alt="Michaela"
+              />
+            </div>
+            <span className="name name-aboutus">Michaela Kantorová</span>
+          </figure>
+        </div>
+
+        <div className="figure-aboutus">
+          <figure className="figure">
+            <div className="img-profile-wrapper">
+              <img
+                className="img-profile img-profile-helena"
+                src={helena}
+                alt="Helena"
+              />
+            </div>
+            <span className="name name-aboutus">Helena Šubrtová</span>
+          </figure>
+        </div>
       </section>
     </div>
   );

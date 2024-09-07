427b41b43e90e7bb69164f324909bede94570ae4
Mariana Sarovic <sarovicmariana@gmail.com>
2024-09-07 20:27:52 +0200

    Vymena obrazkov v hero sekcii. Nastylovana paticka.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import { FaFacebook, FaInstagram } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <footer>
-      <p>Patička</p>
+    <footer className="footer">
+      <section className="footer-links">
+        <a
+          className="social-media-icon"
+          href="https://www.facebook.com/tvojProfil"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaFacebook size={30} />
+        </a>
+        <a
+          className="social-media-icon"
+          href="https://www.instagram.com/tvojProfil"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaInstagram size={30} />
+        </a>
+      </section>
+      <p class="copyright">© 2024 mariana sarovic</p>
     </footer>
   );
 };

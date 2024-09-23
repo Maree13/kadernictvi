3eacdd259731eaf3ae735a0098a4dc9b9b63a6f8
Mariana Sarovic <sarovicmariana@gmail.com>
2024-09-23 14:21:56 +0200

    Upravy error page

--- src/App.js
+++ src/App.js
@@ -7,10 +7,22 @@ import Kadernictvi from './pages/Kadernictvi';
 import Kozmetika from './pages/Kozmetika';
 import Aboutus from './pages/Aboutus';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
+  const [windowSize, setWindowSize] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize(window.innerWidth); // Aktualizuje stav pri zmene veľkosti okna
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
-    <BrowserRouter>
+    <BrowserRouter key={windowSize}>
       <Routes>
         <Route element={<SharedLayout />}>
           <Route path="/" element={<Home />} />
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -2,11 +2,13 @@ import { Link } from 'react-router-dom';
 
 const Error = () => {
   return (
-    <section>
+    <section className="error-section">
       <h2>404</h2>
-      <p>Stránka nenalezena</p>
+      <p>I věci, které milujeme, se někdy rozbijí...</p>
       <p>
-        <Link to="/">Zpět na úvodní stranu</Link>
+        <Link className="error-link" to="/">
+          Opravme to společně – vraťme se na začátek
+        </Link>
       </p>
     </section>
   );

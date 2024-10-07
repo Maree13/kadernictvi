a205af82b3f6fbd42f0c1e8197b995de2bd5c214
Mariana Sarovic <sarovicmariana@gmail.com>
2024-10-07 22:54:41 +0200

    upravy kosmetika

--- src/pages/Kadernictvi.js
+++ src/pages/Kadernictvi.js
@@ -1,8 +1,44 @@
 const Kadernictvi = () => {
+  const services = [
+    { name: 'Foukaná', price: 'od 500 Kč' },
+    { name: 'Dámský střih', price: 'od 1000 Kč' },
+    { name: 'Barvení + střih', price: 'od 1700 Kč' },
+    {
+      name: 'Melírování + střih',
+      price: 'od 1000 Kč / hodina',
+      duration: '2,5 hod.',
+    },
+    {
+      name: 'Balayage + střih',
+      price: 'od 1000 Kč / hodina',
+      duration: 'od 3 hodin',
+    },
+    { name: 'Hloubková kůra', price: 'od 700 Kč' },
+    { name: 'Speciální kůry, masky', price: 'od 200 Kč' },
+    { name: 'Večerní, slavnostní účes', price: 'od 1000 Kč' },
+    { name: 'Pánský střih', price: 'od 500 Kč' },
+    { name: 'Dětský střih do 12 let', price: 'od 300 Kč' },
+    { name: 'Střih el. strojkem', price: 'od 400 Kč' },
+  ];
+
   return (
-    <section className="kadernictvi">
-      <h3>In progress :-) </h3>
-    </section>
+    <div className="kadernictvi">
+      <h2 className="kadernictvi-h2">Ceník kadeřnických služeb</h2>
+      <table class="table-cennik-kadernictvi">
+        <tbody>
+          {services.map(({ name, price, duration }, index) => (
+            <tr key={index}>
+              <td className="service-name">{name}</td>
+              <td className="service-price">{price}</td>
+              {/* <td className="service-duration">{duration || '—'}</td> */}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <p className="kadernictvi-paragraph">
+        Ceny služeb jsou pouze orientační, dle spotřeby materiálu.
+      </p>
+    </div>
   );
 };
 
--- src/pages/Kozmetika.js
+++ src/pages/Kozmetika.js
@@ -1,5 +1,18 @@
 const Kozmetika = () => {
-  return <section className="kozmetika">Kozmetika</section>;
+  return (
+    <div className="kozmetika">
+      <h2 className="kozmetika-h2">Kosmetické služby</h2>
+      <p className="kozmetika-paragraph">
+        Péče o suchou, normální, smíšenou nebo mastnou pleť. Ošetření zahrnuje
+        odlíčení pleti, úpravu a případné barvení obočí, gommage, čištění pleti,
+        aplikaci ampule se sérem, masáž obličeje, masku, sérum dle typu pleti a
+        závěrečný krém.
+      </p>
+      <p className="kozmetika-paragraph">
+        Ošetření pleti kosmetikou <strong>Payot</strong>.
+      </p>
+    </div>
+  );
 };
 
 export default Kozmetika;
